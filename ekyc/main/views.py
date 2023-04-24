becd8a1ca5c24f52aabc6a634cbb847aabc20c00
CRYP70 BOSS <ceo@iwando.com>
2023-04-24 15:40:39 +0200

    update

--- ekyc/main/views.py
+++ ekyc/main/views.py
@@ -7,63 +7,6 @@ from .models import *
 from .ml import *
 from decouple import config
 
-def index(request):
-    return render(request, 'index.html')
-
-def register(request):
-    if request.method == 'POST':
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        username = request.POST['username']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-
-        if password1 == password2:
-            if userModel.User.objects.filter(username=username).exists():
-                print('Usernme exists')
-                messages.info(request, 'Username exists')
-                return redirect('register')
-            elif userModel.User.objects.filter(email=email).exists():
-                print('email exists')
-                messages.info(request, 'Email exists')
-                return redirect('register')
-            else:
-                user = userModel.User.objects.create_user(
-                    username=username, email=email, password=password1, first_name=fname, last_name=lname)
-                user.save()
-                return redirect('login')
-        else:
-            print('pass dosent match')
-            messages.info(request, 'Password didn\'t match')
-            return redirect('register')
-        return redirect('/')
-    else:
-        return render(request, "register.html")
-
-def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = userModel.auth.authenticate(username=username, password=password)
-
-        if user is not None:
-            userModel.auth.login(request, user)
-            return redirect('/')
-        else:
-            messages.info(request, 'invalid details')
-            return redirect('login')
-    else:
-        return render(request, "login.html")
-    return render(request, "login.html")
-
-def logout(request):
-    userModel.auth.logout(request)
-    return redirect('/')
-
-def video(request):
-    return render(request, "video.html")
 
 def verify_ids(request):
     if request.method == 'POST':
@@ -91,12 +34,11 @@ def verify_ids(request):
     else:
         return render(request, 'aadharPan.html')
 
-@login_required(login_url='login')
 def verify_phone(request):
     if request.method == 'POST':
         phone = request.POST['phone']
         print(phone)
-        if len(phone) == 10 and phone.isdigit():
+        if len(phone) < 15:
             import twilio
             # Download the helper library from https://www.twilio.com/docs/python/install
             from twilio.rest import Client
@@ -166,13 +108,6 @@ def verify_docs(request):
     else:
         return render(request, 'documents.html')
 
-
-def profile(request):
-    prof = Profile.objects.get(user_id=request.user.id)
-    return render(request, 'profile.html', {'prof':prof})
-
-# def video(request):
-    # return render(request, 'video.html')
 
 
 import base64
